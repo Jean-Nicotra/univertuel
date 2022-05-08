0672614c62b85f3a2453f644ce97af3aa5c18012
jean <jeannicotra@gmail.com>
2022-05-09 00:58:27 +0200

    update

---
+++ src/Form/Game/Prophecy/Game/Caste/ProphecyFormBenefitCampaignType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Form\Game\Prophecy\Game\Caste;
+
+use App\Entity\Game\Prophecy\Game\Caste\ProphecyBenefit;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use App\Entity\Game\Prophecy\Game\Caste\ProphecyCaste;
+use App\Entity\Game\Campaign;
+
+class ProphecyFormBenefitCampaignType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class)
+            ->add('caste', EntityType::class, [
+                'class' => ProphecyCaste::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false,    
+                
+            ])
+            ->add('campaign', EntityType::class, [
+                'class' => Campaign::class,
+                'choice_label' => 'name',
+                'multiple' => false,
+                'expanded' => false,
+                
+            ])
+            ->add('description', TextareaType::class)
+            ->add('valider', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ProphecyBenefit::class,
+        ]);
+    }
+}
